0d20536ea1d1bf32b70d47afbcfd6352fae2e0b5
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-31 18:44:19 +0300

    Update __main__.py

--- webss/__main__.py
+++ webss/__main__.py
@@ -8,6 +8,20 @@ from loguru import logger
 from configparser import ConfigParser
 from os import environ
 
+config = ConfigParser()
+config.read('config.ini')
+
+browser = None
+is_browser_started = False
+
+async def start_browser():
+    global browser, is_browser_started
+    browser = await pyppeteer.launch(
+            headless=True,
+            args=['--no-sandbox', '--disable-setuid-sandbox']
+            )
+    is_browser_started = True
+
 
 def get_var(name, default=None):
     ENV = bool(environ.get('ENV', False))
@@ -41,13 +55,18 @@ async def endpoint(site: str):
     __Exception:__
         `[error]`: `[pyppeteer.errors]`
     """
+    global browser, is_browser_started
+    if not is_browser_started:
+        print("started")
+        await start_browser()
+        is_browser_started = True
+    page = await browser.newPage()
     if re.match(r'^https?://', site):
         url = site
     else:
         url = 'http://'+site
-    browser = await pyppeteer.launch(headless=True)
     page = await browser.newPage()
-    file tempfile() + ".png"
+    file = tempfile() + ".png"
     try:
         await page.goto(url)
         await page.setViewport({'width': 1280, 'height': 720})
