d7a18361c45c4a58c2a2ee7180bb4ad21980dfa0
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-31 13:16:20 +0300

    Update __main__.py

--- webss/__main__.py
+++ webss/__main__.py
@@ -1,38 +1,61 @@
-import asyncio
-from .import app, host, port
+import sys, re
 import pyppeteer
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from typing import Optional
 from temp import tempfile
 import uvicorn
+from loguru import logger
+from configparser import ConfigParser
+from os import environ
+
+
+def get_var(name, default=None):
+    ENV = bool(environ.get('ENV', False))
+    if ENV:
+        return environ.get(name, default)
+
+    try:
+        return config.get('web', name)
+    except AttributeError:
+        return None
+
+
+host = get_var('host', 'localhost')
+port = int(get_var('port', 8869))
+
+
+logger.add(sys.stdout, colorize=True, format="<green>{time:HH:mm:ss}</green> | {level} | <level>{message}</level>")
+
+
+app = FastAPI()
 
 
 @app.get("/")
 async def endpoint(site: str):
     """__[Web-SS Endpoint]__
-
     __Args:__
         `site (str)`: `[website url]`
-
     __Returns:__
         `[type]`: `[FileResponse]` = image: mime_type: image/png
     
     __Exception:__
         `[error]`: `[pyppeteer.errors]`
     """
-    browser = await pyppeteer.launch(
-        headless=True
-    )
+    if re.match(r'^https?://', site):
+        url = site
+    else:
+        url = 'http://'+site
+    browser = await pyppeteer.launch(headless=True)
     page = await browser.newPage()
-    file = tempfile() + ".png"
+    file tempfile() + ".png"
     try:
-        await page.goto(site)
+        await page.goto(url)
+        await page.setViewport({'width': 1280, 'height': 720})
     except pyppeteer.errors.NetworkError as e:
-        return {
-            "error": str(e)
-        }
+        return {"error": str(e)}
     await page.screenshot({'path': file})
     return FileResponse(file)
 
+
 if __name__ == "__main__":
-    uvicorn.run("webss:app", host=host, port=port, log_level="info")
+    uvicorn.run(app=app, host=host, port=port)
